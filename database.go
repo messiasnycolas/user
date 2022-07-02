19e3e9a07bbf8dd8174bd4dd15b2d9462ad9dc12
Nycolas Messias <nycolas.messias@sossego.com.br>
2022-07-01 23:22:34 -0300

    chore: set DB vars in environment

--- database.go
+++ database.go
@@ -4,14 +4,18 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var dburl string = "root:123456@/golearning"
+var dbUsername = os.Getenv("DB_USERNAME")
+var dbPassword = os.Getenv("DB_PASSWORD")
+var dbName = os.Getenv("DB_NAME")
+var dbUrl string = fmt.Sprintf("%v:%v@/%v", dbUsername, dbPassword, dbName)
 
 func openDBConnection(w http.ResponseWriter) (db *sql.DB) {
-	db, err := sql.Open("mysql", dburl)
+	db, err := sql.Open("mysql", dbUrl)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "Oops! There is something wrong with my DB.")
